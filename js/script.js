8806c7ad12418d926729cf1dd5f4db42874abc43
Kronzii88 <ezyh.arofi@gmail.com>
2020-12-28 07:47:40 +0700

    animasi intro keluar

--- js/script.js
+++ js/script.js
@@ -15,6 +15,21 @@ const buttonAnimation = () => {
                     $(this).velocity({width:125})
                 })
 }
+
+const introOut = () => {
+    $(".start").velocity("transition.whirlOut", {
+                stagger: 150, 
+                complete: () => {
+                   $(".text").velocity({"font-size":"20px"},{
+                                            duration: 1000
+                                        });
+                    $(".intro").velocity({"top":"95%"},{
+                                        duration: 1000
+                                        });
+                }
+            });
+}
+
 $(document).ready(() =>  {
     introAnimation();
 })
