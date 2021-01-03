d4c87f988956a9ee280860a4f8147d3b0908ab86
Kronzii88 <ezyh.arofi@gmail.com>
2021-01-03 14:59:17 +0700

    finishing

--- js/script.js
+++ js/script.js
@@ -17,6 +17,9 @@ const buttonAnimation = () => {
 }
 
 const introOut = () => {
+    // proteksi tombol start
+    $(".start").attr("disabled", true).css({"color":"black"});
+
     $(".start").velocity("transition.whirlOut", {
                 stagger: 150, 
                 complete: () => {
@@ -28,6 +31,7 @@ const introOut = () => {
                                         complete: () => {
                                             callMenu();
                                             $(".menu a[href='about_me']").trigger("click");
+                                            $(".start").attr("disabled", false).css({"color":"black"});
                                         } 
                                         });
                 }
@@ -39,16 +43,33 @@ const callMenu = () => {
         stagger: 250, 
        });
     
-    $(".menu a").click(function(e) {
+    $(".menu a").off().click(function(e) {
         e.preventDefault(); 
         $(this).parent("li").addClass("active").siblings().removeClass("active");
 
-        //memanggil fungsi sesuai dengan nilai href
+        
         const hrefString = $(this).attr("href");
         // console.log(hrefString);
-        $("#" + hrefString).show();
 
-        window[hrefString]();
+        if(hrefString == "contact") {
+            contact()
+        }else {
+            if(!$(`#${hrefString}`).is(":visible")){
+                //menghilangkan konten sebelum konten berikutnya muncul
+                 $(".container-content").fadeOut(1000);
+     
+                 //asyncrhonus timeout digunakan agar animasi fading dan showin tidak bertabrakan
+                 setTimeout(() => {
+                     //memanggil fungsi sesuai dengan nilai href
+                     $(`#${hrefString}`).show();
+                     window[hrefString]();
+                 }, 1000)
+     
+             } 
+        }
+        
+  
+
     })
 }
 
@@ -56,6 +77,28 @@ function about_me(){
     $("#about_me img").velocity("transition.flipYIn", {duration: 1500});
     $("#about_me .title").velocity("transition.slideUpIn", {duration: 1500});
     $("#about_me div").velocity("transition.slideDownIn", {duration: 1500});
+}
+
+function timeline() {
+    $(".top240").velocity("transition.slideUpIn", {stagger: 1000});
+    $(".top170").velocity("transition.slideDownIn", {stagger: 1000});
+}
+
+function contact() {
+    $(".menu li").hide();
+    $(".container-content").hide();
+
+        $(".text").velocity({"font-size":"90px"},{
+                                duration: 1000
+                                });
+        $(".intro").velocity({"top":"50%"},{
+                            duration: 1000,
+                            complete: () => {
+                                
+                                $(".start").velocity("transition.whirlIn");
+                            } 
+                            });
+
 }
 
 $(document).ready(() =>  {
